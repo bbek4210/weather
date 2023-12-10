dfebfd1cdd4c774394adde821a6054d3a05cff27
bbek4210 <bhandaribibek731@gmail.com>
2023-12-10 22:53:27 +0545

    n

--- src/components/weather/temp.js
+++ src/components/weather/temp.js
@@ -10,7 +10,7 @@ const Temp = () => {
 
   const getWeatherInfo = async () => {
     try {
-      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={WriteYourAPIKey}`;
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=0383bf80d9d0461726f523188443819c`;
 
       let res = await fetch(url);
       let data = await res.json();
